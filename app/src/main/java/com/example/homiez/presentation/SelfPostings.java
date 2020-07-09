71c0273205d758cacbc386b91e3801e1923bc539
Jordan Portz <portzj@myumanitoba.ca>
2020-07-08 21:36:28 -0500

    The self postings page now displays all the postings for the given user hard coded until the Bundle is working, and properly displays the information

--- app/src/main/java/com/example/homiez/presentation/SelfPostings.java
+++ app/src/main/java/com/example/homiez/presentation/SelfPostings.java
@@ -6,11 +6,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.homiez.R.layout;
 import com.example.homiez.R.id;
+import com.example.homiez.application.Main;
 import com.example.homiez.objects.Posting;
 import com.example.homiez.business.AccessPostings;
 
@@ -18,26 +22,36 @@ public class SelfPostings extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        System.out.println("testing ASDNAKSLUJFBGIASFJB:AISKBFJKASBFASJKF:B");
+        Main.startUp();
+
         super.onCreate(savedInstanceState);
         setContentView(layout.self_postings);
 
 //        Bundle b = getIntent().getExtras();
 //        String userID = b.getString("userID");
 
-        final ArrayList<String> test = new ArrayList<>();
+        final ArrayList<Posting> postings = new ArrayList<>();
+        AccessPostings accessPostings = new AccessPostings();
 
-        AccessPostings aas = new AccessPostings();
+        accessPostings.getPostingsByUserId(postings, "1");
 
-        test.add("Horse");
-        test.add("Cow");
-        test.add("Camel");
-        test.add("Sheep");
-        test.add("Goat");
+        final ArrayAdapter<Posting> adapter = new ArrayAdapter<Posting>(this, android.R.layout.simple_list_item_2, android.R.id.text1, postings)
+        {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
 
-//        ArrayAdapter a = new ArrayAdapter(this, android.R.layout.simple_list_item_1, test);
+                TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+                TextView text2 = (TextView) view.findViewById(android.R.id.text2);
 
-//        ListView listView = (ListView)findViewById(id.listStudentCourses);
-//        listView.setAdapter(a);
+                text1.setText(postings.get(position).getTitle());
+                text2.setText(postings.get(position).getLocation() + ", $" + postings.get(position).getPrice());
+
+                return view;
+            }
+        };
+
+        ListView listView = (ListView)findViewById(id.postingsList);
+        listView.setAdapter(adapter);
     }
 }
