487ee6f77a98d07ef621c79ddcaf6805a9f36cc1
mattSmith215 <smithm28@myumanitoba.ca>
2020-07-25 23:05:43 -0500

    AAAAAAAAA

--- app/sql/PopulateUsers.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO Users (name, age, gender, budget, description) VALUES ("Abhi", 20, 'm', 2000, "test decription Abhi");
-INSERT INTO Users (name, age, gender, budget, description) VALUES ("Matt", 21, 'm', 1500, "test decription Matt");
-INSERT INTO Users (name, age, gender, budget, description) VALUES ("Ma", 19, 'm', 2200, "test decription Ma");
-INSERT INTO Users (name, age, gender, budget, description) VALUES ("Vinh", 18, 'm', 2500, "test decription Vinh");
-INSERT INTO Users (name, age, gender, budget, description) VALUES ("Jordan", 22, 'm', 1000, "test decription Jordan");
-INSERT INTO Users (name, age, gender, budget, description) VALUES ("John", 25, 'm', 800, "test decription John");
-INSERT INTO Users (name, age, gender, budget, description) VALUES ("Linda", 30, 'm', 3000, "test decription Linda");
