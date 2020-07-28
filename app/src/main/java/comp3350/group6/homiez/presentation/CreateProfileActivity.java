fdf45e5856f2ecc124dd24f3f0626be840cfe045
Vinh Tran <vinhtran.k@gmail.com>
2020-07-28 13:10:54 -0500

    Created CreatedProfileActivity

---
+++ app/src/main/java/comp3350/group6/homiez/presentation/CreateProfileActivity.java
@@ -0,0 +1,13 @@
+package comp3350.group6.homiez.presentation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class CreateProfileActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
