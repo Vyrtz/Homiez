894f05544facf77bf165acb9fa52869e8879a292
Vinh Tran <vinhtran.k@gmail.com>
2020-07-28 15:04:06 -0500

    Fields added to create_profile.xml

--- app/src/main/java/comp3350/group6/homiez/presentation/CreateProfileActivity.java
+++ app/src/main/java/comp3350/group6/homiez/presentation/CreateProfileActivity.java
@@ -1,13 +1,35 @@
 package comp3350.group6.homiez.presentation;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import comp3350.group6.homiez.R;
+import comp3350.group6.homiez.application.Main;
+
+import java.util.Calendar;
 
 
 public class CreateProfileActivity extends Activity {
 
+    //TODO: Change the manifest back once done testing
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Main.startUp();
+
+        setContentView(R.layout.create_profile);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Main.shutDown();
+    }
+
 }
