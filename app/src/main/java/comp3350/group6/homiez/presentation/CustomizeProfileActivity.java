9c76c47c76cea7e82ab219f1369efbba00f8a591
Vinh Tran <vinhtran.k@gmail.com>
2020-07-29 23:08:11 -0500

    Linked the profileFragment to customizeProfile

---
+++ app/src/main/java/comp3350/group6/homiez/presentation/CustomizeProfileActivity.java
@@ -0,0 +1,16 @@
+package comp3350.group6.homiez.presentation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import comp3350.group6.homiez.R;
+
+public class CustomizeProfileActivity extends Activity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.customize_profile);
+    }
+}
--- app/src/main/java/comp3350/group6/homiez/presentation/ProfileFragment.java
+++ app/src/main/java/comp3350/group6/homiez/presentation/ProfileFragment.java
@@ -1,10 +1,11 @@
 package comp3350.group6.homiez.presentation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -18,12 +19,13 @@ import comp3350.group6.homiez.objects.Interest;
 import comp3350.group6.homiez.objects.Posting;
 import comp3350.group6.homiez.objects.User;
 
-public class ProfileFragment extends Fragment{
+public class ProfileFragment extends Fragment implements View.OnClickListener{
 
     private AccessUser accessUser;
     private ArrayList<Posting> postings;
     private AccessPostings accessPostings;
     private User user;
+    private Boolean customize;
 
     final private String CUSTOMIZE = "Customize";
     final private String SUBMIT = "Submit Changes";
@@ -35,6 +37,8 @@ public class ProfileFragment extends Fragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        customize = false;
+
         //Grab the user
         Bundle b = this.getArguments();
         accessUser = new AccessUser();
@@ -42,13 +46,17 @@ public class ProfileFragment extends Fragment{
 
         View v = inflater.inflate(R.layout.profile, container, false);
 
-        //Gather variables for fields that are going to be changed
+        //Initialize UI element variables
         TextView header = v.findViewById(R.id.header);
-        EditText name = v.findViewById(R.id.name);
-        EditText age = v.findViewById(R.id.age);
-        EditText gender = v.findViewById(R.id.gender);
-        EditText biography = v.findViewById(R.id.bio);
-        EditText interests = v.findViewById(R.id.interests);
+        TextView name = v.findViewById(R.id.name);
+        TextView age = v.findViewById(R.id.age);
+        TextView gender = v.findViewById(R.id.gender);
+        TextView biography = v.findViewById(R.id.bio);
+        TextView interests = v.findViewById(R.id.interests);
+
+        //Set up button listener
+        Button customizeButt = v.findViewById(R.id.customizeButt);
+        customizeButt.setOnClickListener(this);
 
         //Filling default values for the profile
         header.setText(user.getName() + HEADER_SUFFIX);
@@ -72,17 +80,32 @@ public class ProfileFragment extends Fragment{
         interests.setText(interestText);
 
 
+        //TODO: Populate the posting list
+
+        //ListView postingsList = v.findViewById(R.id.postingList);
+
         //Initialize DB access and a postings list
-        postings = new ArrayList<>();
-        accessPostings = new AccessPostings();
+        //postings = new ArrayList<>();
+        //accessPostings = new AccessPostings();
 
         //Populate postings
-        accessPostings.getPostingsByUserId(postings, user.getUserId());
+        //accessPostings.getPostingsByUserId(postings, user.getUserId());
 
-
+        //TODO: Condition check on the user - otherwise there would be no button
+        customizeButt.setVisibility(View.VISIBLE);
 
 
 
         return v;
     }
+
+    public void onClick(View view) {
+        Intent startIntent = new Intent(getActivity(), CustomizeProfileActivity.class);
+        Bundle b = this.getArguments();
+        startIntent.putExtras(b);
+        startActivity(startIntent);
+
+    }
+
+
 }
