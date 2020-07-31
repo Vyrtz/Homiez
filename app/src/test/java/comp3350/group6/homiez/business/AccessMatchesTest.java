47390da653e17c7c430bfb368f096b3e308325e6
Abhishek Sachdev <sachdevabhi74@gmail.com>
2020-07-30 19:20:51 -0500

    tests_AccessMatches

--- app/src/test/java/comp3350/group6/homiez/business/AccessMatchesTest.java
+++ app/src/test/java/comp3350/group6/homiez/business/AccessMatchesTest.java
@@ -12,70 +12,94 @@ import java.util.List;
 public class AccessMatchesTest extends TestCase {
 
     private AccessMatches aMatches;
+    private ArrayList<Match> matches;
     public void setUp() {
         Services.createDataAccess(new DataAccessStub("test"));
         aMatches = new AccessMatches();
+        matches = new ArrayList<Match>();
     }
 
     public void testAccessMatchesValidConstruction() {
         System.out.println("\nStarting testAccessMatchesConstruction");
-        ArrayList<Match> matches = new ArrayList<Match>();
+
         aMatches.getMatchesForUser(matches, "0");
         assertEquals(0, matches.size(), 0.01);
+
         System.out.println("Finished testAccessMatchesConstruction");
-    }
-    public void testAccessMatchesInvalidConstruction() {
-        Services.closeDataAccess();
-        System.out.println("\nStarting testAccessMatchesInvalidConstruction");
-        aMatches = new AccessMatches();
-        System.out.println("Finished testAccessMatchesInvalidConstruction");
     }
 
     public void testNullValues() {
-        AccessMatches aMatches = new AccessMatches();
-        ArrayList<Match> matches = new ArrayList<Match>();
-        Match m = new Match("0", "4");
+        System.out.println("\nStarting testNullValues");
 
-        aMatches.getMatchesForPosting(matches, "0");
-        assertEquals(1, matches.size(), 0.01);
-        matches.removeAll(matches);
+        aMatches.getMatchesForPosting(matches, null);
+        assertEquals(0, matches.size(), 0.01);
+        matches.clear();
+
+        aMatches.getMatchesForUser(matches, null);
+        assertEquals(0, matches.size(), 0.01);
+        matches.clear();
+
+        assertNull(aMatches.insertMatch( null));
+        assertNull(aMatches.deleteMatch( null));
+
+        System.out.println("Finished testNullValues");
+    }
+    public void testBadValues() {
+        System.out.println("\nStarting testBadValues");
+
+        aMatches.getMatchesForPosting(matches, "-10");
+        assertEquals(0, matches.size(), 0.01);
+        matches.clear();
+
+        aMatches.getMatchesForUser(matches, "-10");
+        assertEquals(0, matches.size(), 0.01);
+        matches.clear();
+
+        assertNull(aMatches.deleteMatch( new Match("09", "100")));
+        System.out.println("Finished testBadValues");
+    }
+
+    public void testNonExistingMatches() {
+        System.out.println("\nStarting testNonExistingMatches");
+
+        Match m = new Match("0", "4");
         aMatches.getMatchesForPosting(matches, "4");
         assertEquals(0, matches.size(), 0.01);
 
-        aMatches.getMatchesForUser(matches, "3");
-        assertEquals(2, matches.size(), 0.01);
-        matches.removeAll(matches);
+        assertNull(aMatches.deleteMatch( new Match("09", "100")));
+
         aMatches.getMatchesForUser(matches, "0");
         assertEquals(0, matches.size(), 0.01);
+        matches.clear();
 
         aMatches.insertMatch(m);
         aMatches.getMatchesForUser(matches, "0");
         assertEquals(1, matches.size(), 0.01);
-        matches.removeAll(matches);
+        matches.clear();
 
         aMatches.deleteMatch(m);
         aMatches.getMatchesForUser(matches, "0");
         assertEquals(0, matches.size(), 0.01);
 
-
-        System.out.println("Finished testAccessMatches1");
+        System.out.println("Finished testNonExistingMatches");
 
     }
-//
-//    public String getMatchesForUser(List<Match> matches, String userId) {
-//        return dataAccess.getMatchesForUser(matches, userId);
-//    }
-//
-//    public String getMatchesForPosting(List<Match> matches, String postingId) {
-//        return dataAccess.getMatchesForPosting(matches, postingId);
-//    }
-//    public String insertMatch(Match currentMatch)
-//    {
-//        return dataAccess.insertMatch(currentMatch);
-//    }
-//
-//    public String deleteMatch(Match currentMatch)
-//    {
-//        return dataAccess.deleteMatch(currentMatch);
-//    }
+    public void testExistingMatches() {
+        System.out.println("\nStarting testExistingMatches");
+
+        Match m = new Match("0", "4");
+
+        aMatches.getMatchesForUser(matches, "3");
+        assertEquals(2, matches.size(), 0.01);
+        m = matches.get(0);
+        matches.clear();
+
+        aMatches.deleteMatch(m);
+        aMatches.getMatchesForUser(matches, "3");
+        assertEquals(1, matches.size(), 0.01);
+
+
+        System.out.println("Finished testExistingMatches");
+
+    }
 }
