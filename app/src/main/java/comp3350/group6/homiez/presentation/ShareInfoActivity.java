444d450ffcac55e1ef9a74aa2e8edb2f3fea7fb0
Vinh Tran <vinhtran.k@gmail.com>
2020-08-11 15:52:26 -0500

    Created ShareInfoActivity.java

---
+++ app/src/main/java/comp3350/group6/homiez/presentation/ShareInfoActivity.java
@@ -0,0 +1,48 @@
+package comp3350.group6.homiez.presentation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import comp3350.group6.homiez.R;
+import comp3350.group6.homiez.business.AccessUser;
+import comp3350.group6.homiez.objects.User;
+
+public class ShareInfoActivity extends Activity {
+
+    private AccessUser accessUser;
+    private User user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.share_info);
+
+        Bundle b = getIntent().getExtras();
+
+        user = accessUser.getUser(b.getString("userID"));
+
+        //Populate the fields
+        TextView phoneNum = findViewById(R.id.phone);
+        TextView email = findViewById(R.id.email);
+
+        //TODO: Database changes for phone and email
+        //phoneNum.setText(user.getPhone());
+        //email.setText(user.getEmail());
+
+    }
+
+    public void viewProfile(View v){
+        Intent startIntent = new Intent(ShareInfoActivity.this, PublicProfileActivity.class);
+        Bundle bundle = getIntent().getExtras();
+        bundle.putString("profileID", user.getUserId());
+        startIntent.putExtras(bundle);
+        startActivity(startIntent);
+    }
+
+
+
+}
