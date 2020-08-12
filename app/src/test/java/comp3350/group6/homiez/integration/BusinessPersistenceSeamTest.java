b44bcbe78f388a44b12307335a54703e3221f21c
Vinh Tran <vinhtran.k@gmail.com>
2020-08-12 14:12:14 -0500

    Created integration package, setup BusinessPersistenceSeamTest.java

---
+++ app/src/test/java/comp3350/group6/homiez/integration/BusinessPersistenceSeamTest.java
@@ -0,0 +1,24 @@
+package comp3350.group6.homiez.integration;
+
+import junit.framework.TestCase;
+
+public class BusinessPersistenceSeamTest extends TestCase {
+
+    public BusinessPersistenceSeamTest(String arg0) { super(arg0); }
+
+    public void testAccessMatches() {
+
+    }
+
+    public void testAccessPostings() {
+
+    }
+
+    public void testAccessRequests() {
+
+    }
+
+    public void testAccessUser() {
+
+    }
+}
---
+++ app/src/test/java/comp3350/group6/homiez/integration/IntegrationTests.java
@@ -0,0 +1,19 @@
+package comp3350.group6.homiez.integration;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class IntegrationTests {
+
+    public static TestSuite suite;
+
+    public static Test suite() {
+        suite = new TestSuite("Integration tests");
+
+        //TODO: Modify the addTest suite calls once the tests are written
+        //suite.addTestSuite();
+        //suite.addTestSuite();
+        return suite;
+
+    }
+}
