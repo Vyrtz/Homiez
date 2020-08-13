6d4fd5707d44a0769e6b5cbb0f4a14d1c304c28b
Jordan Portz <portzj@myumanitoba.ca>
2020-08-13 16:38:41 -0500

    Set up running all test file

---
+++ app/src/androidTest/java/RunAcceptanceTests.java
@@ -0,0 +1,14 @@
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import comp3350.group6.homiez.presentation.AcceptanceTests;
+
+public class RunAcceptanceTests {
+    public static TestSuite suite;
+
+    public static Test suite() {
+        suite = new TestSuite("Acceptance tests");
+        suite.addTest(AcceptanceTests.suite());
+        return suite;
+    }
+}
