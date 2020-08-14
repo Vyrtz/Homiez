48894b36f4d277437f4c921eaa91578dce42ffda
mattSmith215 <smithm28@myumanitoba.ca>
2020-08-13 23:22:43 -0500

    Send and view requests done

--- app/src/androidTest/java/comp3350/group6/homiez/presentation/MatchesTest.java
+++ app/src/androidTest/java/comp3350/group6/homiez/presentation/MatchesTest.java
@@ -6,19 +6,13 @@ import android.view.ViewParent;
 
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.ViewInteraction;
-import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import java.util.ArrayList;
 
 import comp3350.group6.homiez.R;
 import comp3350.group6.homiez.business.AccessRequests;
@@ -27,16 +21,12 @@ import comp3350.group6.homiez.objects.Request;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
-import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
 
 
 public class MatchesTest {
@@ -44,34 +34,7 @@ public class MatchesTest {
     @Rule
     public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
 
-    @Test
-    public void sendRequestTest() {
 
-
-        //Login
-        onView(withId(R.id.editUserID)).perform(clearText(), typeText("4"));
-        Espresso.pressBack();
-        onView(withId(R.id.editPassword)).perform(clearText(), typeText("dev"));
-        Espresso.pressBack();
-        onView(withId(R.id.loginButton)).perform(click());
-
-        onView(withContentDescription("Room at Bridgewater Apartment")).perform(click());
-        onView(withContentDescription("Send Request")).perform(click());
-        Espresso.pressBack();
-        Espresso.pressBack();
-
-        //Login
-        onView(withId(R.id.editUserID)).perform(clearText(), typeText("2"));
-        Espresso.pressBack();
-        onView(withId(R.id.editPassword)).perform(clearText(), typeText("dev"));
-        Espresso.pressBack();
-        onView(withId(R.id.loginButton)).perform(click());
-
-        onView(withContentDescription("Your Rooms")).perform(click());
-        onView(withContentDescription("Requests")).perform(click());
-
-
-    }
 
 
     private static Matcher<View> childAtPosition(
