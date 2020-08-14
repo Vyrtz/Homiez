7ce5b443bc03ce68500a26b319e6523afab2a63b
Vinh Tran <vinhtran.k@gmail.com>
2020-08-13 22:47:40 -0500

    Created denyRequestTest

--- app/src/androidTest/java/comp3350/group6/homiez/presentation/MatchesTest.java
+++ app/src/androidTest/java/comp3350/group6/homiez/presentation/MatchesTest.java
@@ -1,42 +1,22 @@
 package comp3350.group6.homiez.presentation;
 
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
 import androidx.test.espresso.Espresso;
-import androidx.test.espresso.ViewInteraction;
-import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import java.util.ArrayList;
 
 import comp3350.group6.homiez.R;
-import comp3350.group6.homiez.business.AccessRequests;
-import comp3350.group6.homiez.objects.Request;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
-import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
 
 
 public class MatchesTest {
@@ -46,8 +26,6 @@ public class MatchesTest {
 
     @Test
     public void sendRequestTest() {
-
-
         //Login
         onView(withId(R.id.editUserID)).perform(clearText(), typeText("4"));
         Espresso.pressBack();
@@ -55,42 +33,34 @@ public class MatchesTest {
         Espresso.pressBack();
         onView(withId(R.id.loginButton)).perform(click());
 
-        onView(withContentDescription("Room at Bridgewater Apartment")).perform(click());
+        onView(withText("Room at Bridgewater Apartment")).perform(click());
         onView(withContentDescription("Send Request")).perform(click());
         Espresso.pressBack();
         Espresso.pressBack();
 
         //Login
-        onView(withId(R.id.editUserID)).perform(clearText(), typeText("2"));
+       /* onView(withId(R.id.editUserID)).perform(clearText(), typeText("2"));
         Espresso.pressBack();
         onView(withId(R.id.editPassword)).perform(clearText(), typeText("dev"));
         Espresso.pressBack();
         onView(withId(R.id.loginButton)).perform(click());
 
         onView(withContentDescription("Your Rooms")).perform(click());
-        onView(withContentDescription("Requests")).perform(click());
+        onView(withContentDescription("Requests")).perform(click());*/
 
 
     }
 
+    @Test
+    public void denyRequestTest() {
+        //Login
+        onView(withId(R.id.editUserID)).perform(clearText(), typeText("0"));
+        Espresso.pressBack();
+        onView(withId(R.id.editPassword)).perform(clearText(), typeText("dev"));
+        Espresso.pressBack();
+        onView(withId(R.id.loginButton)).perform(click());
 
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
+        
     }
 
 }
